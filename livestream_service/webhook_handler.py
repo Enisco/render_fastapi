bdfaec67704526653946c7473c3099aa972a0c6e
Enisco <sodeeqe03@gmail.com>
2025-03-09 13:21:35 +0100

    Fixed livestream_service.livestream

--- livestream_service/webhook_handler.py
+++ livestream_service/webhook_handler.py
@@ -1,6 +1,6 @@
 from typing import Any
 
-from livestream import end_session, start_session, upload_recording
+from livestream_service.livestream import end_session, start_session, upload_recording
 from models.call_session_model import session_json_to_model
 from models.participant_model import participant_json_to_model
 

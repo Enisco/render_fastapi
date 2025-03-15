045c6805130b94e7ce50d5bb26e131c68cd7759e
Enisco <sodeeqe03@gmail.com>
2025-03-15 18:28:30 +0100

    Awaiting save_comment_in_database

--- main.py
+++ main.py
@@ -113,7 +113,7 @@ async def comments_websocket_endpoint(websocket: WebSocket, topic: str):
             # await websocket.send_text(ack_message)
 
             # Save message to GTube Main Database 
-            save_comment_in_database(topic, data)
+            await save_comment_in_database(topic, data)
 
             # Broadcast message to all clients in the topic
             await manager.send_message(topic, data)
